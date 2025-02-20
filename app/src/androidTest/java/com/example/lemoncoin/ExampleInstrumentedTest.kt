f3aebea3c255e61acbc8087559aa9e6d07ea9708
IntiGenovez <inti_genovez@hotmail.com>
2025-02-20 17:17:46 -0300

    commit inicial

--- app/src/androidTest/java/com/example/lemoncoin/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/lemoncoin/ExampleInstrumentedTest.kt
@@ -12,6 +12,8 @@ import org.junit.Assert.*
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
+ *
+ * Fiz uma alteração pra ver se muda no repositório remoto
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
