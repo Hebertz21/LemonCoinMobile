78426916ebec5c9d69027da18790b09b106de73a
Hebertz21 <hebertruan84@gmail.com>
2025-03-07 16:41:22 -0300

    Continuação da tela Cadastro

--- app/src/main/java/com/example/lemoncoin/CadastroActivity.kt
+++ app/src/main/java/com/example/lemoncoin/CadastroActivity.kt
@@ -9,12 +9,16 @@ import androidx.core.view.WindowInsetsCompat
 class CadastroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+       // enableEdgeToEdge()
         setContentView(R.layout.activity_cadastro)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun spinnerMes(){
+
     }
 }
--- app/src/main/java/com/example/lemoncoin/LoginActivity.kt
+++ app/src/main/java/com/example/lemoncoin/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.lemoncoin
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.activity.enableEdgeToEdge
@@ -12,11 +13,15 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.Menu)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
 
+
+    fun toCadastro(view: View) {
+        setContentView(R.layout.activity_main)
+    }
 }
