c11f67c02f4e95d4ade790fbab75f8b90c60bf45
Hebertz21 <hebertruan84@gmail.com>
2025-03-19 16:37:10 -0300

    Inicio fragment Conta

--- app/src/main/java/com/example/lemoncoin/HomeActivity.kt
+++ app/src/main/java/com/example/lemoncoin/HomeActivity.kt
@@ -1,21 +1,20 @@
 package com.example.lemoncoin
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.lemoncoin.databinding.ActivityHomeBinding
+import com.example.lemoncoin.fragments.ContasFragments
 
 class HomeActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityHomeBinding // Declare o binding
+    private lateinit var binding: ActivityHomeBinding //binding criado
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        binding = ActivityHomeBinding.inflate(layoutInflater)
-        setContentView(binding.root) // Use o root do binding
+        binding = ActivityHomeBinding.inflate(layoutInflater)  //inflando o layout da tela
+        setContentView(binding.root)
 
         ViewCompat.setOnApplyWindowInsetsListener(binding.navView) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -23,13 +22,29 @@ class HomeActivity : AppCompatActivity() {
             insets
         }
 
-        binding.btnHome.setOnClickListener(){
-            if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
+        // Configuração do botão para abrir/fechar o menu lateral
+        binding.btnHome.setOnClickListener {
+            if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) { //Se estiver aberta, o bindinf fecha
                 binding.drawerLayout.closeDrawer(GravityCompat.START)
-            }
-            else{
+            } else {                                                      //Se estiver fechada, o binding abre
                 binding.drawerLayout.openDrawer(GravityCompat.START)
             }
         }
+
+        // Configuração do clique para abrir o fragmento Contas
+        binding.txtContas.setOnClickListener { //Função do clique Contas
+            openFragment(ContasFragments())
+        }
+    }
+
+    // Método para abrir fragmentos
+    private fun openFragment(fragment: androidx.fragment.app.Fragment) {
+        supportFragmentManager.beginTransaction() //
+            .replace(R.id.fragmentContas, fragment)
+            .addToBackStack(null) // Permite voltar ao fragmento anterior depois de apertar
+                                        //o voltar do celular
+            .commit()                   //Finaliza a aplicação
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
     }
 }
+
