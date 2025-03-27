1772ec47f6841426c5c30adf024ee019fc36461f
Hebertz21 <hebertruan84@gmail.com>
2025-03-27 17:12:05 -0300

    Tela adicionar despesas feita

--- app/src/main/java/com/example/lemoncoin/fragments/DespesasFragment.kt
+++ app/src/main/java/com/example/lemoncoin/fragments/DespesasFragment.kt
@@ -6,18 +6,35 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.lemoncoin.R
+import com.example.lemoncoin.databinding.FragmentDespesasBinding
 
 class DespesasFragment : Fragment() {
+
+    private var _binding: FragmentDespesasBinding? = null
+    private val binding get() = _binding!! // Uso seguro do binding
 
     override fun onCreateView(  //Metodo que constroi a visualização do fragment
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(   //Cria a visualização
-            R.layout.fragment_despesas,  //R chama o layout, o containerView e
-            container,
-            false // anexa ao elemento raiz automaticamente
-        )
+    ): View {
+        _binding = FragmentDespesasBinding.inflate(inflater, container, false)
+
+        binding.btnAddDespesas.setOnClickListener(){
+            trocarFragment(AddDespesasFragment())
+        }
+
+        return binding.root
     }
+    private fun trocarFragment(fragment: androidx.fragment.app.Fragment) {
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainerRelatorios, fragment)
+            .addToBackStack(null) //
+            .commit()
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null // Evita vazamento de memória
+    }
+
 }
