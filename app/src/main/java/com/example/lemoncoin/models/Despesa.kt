91aad3c31035790e13e8eae2f91ed512520cf7f8
Hebertz21 <hebertruan84@gmail.com>
2025-04-18 14:05:23 -0300

    layout das listas Despesas criadas

---
+++ app/src/main/java/com/example/lemoncoin/models/Despesa.kt
@@ -0,0 +1,10 @@
+package com.example.lemoncoin.models
+
+data class Despesa(
+    val data: String,
+    val valor: String,
+    val nome: String,
+    val categoria: String,
+    val conta: String
+
+)
