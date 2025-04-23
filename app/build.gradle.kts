b59688d6a4bbbd9b35338beda9ab8adaa2e151d2
Hebertz21 <hebertruan84@gmail.com>
2025-04-23 16:41:35 -0300

    Layout da lista de contas feito

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.auth.ktx)
     implementation(libs.firebase.firestore.ktx)
+    implementation(libs.androidx.gridlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
